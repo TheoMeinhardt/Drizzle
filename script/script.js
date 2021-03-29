b9a5e3f4ceff46b22aaef7fed93e93cacf5cd974
TheoMeinhardt <meinhardt.t04@htlwienwest.at>
2021-03-29 20:33:29 +0200

    V2021.03.29

--- script/script.js
+++ script/script.js
@@ -14,7 +14,7 @@ async function GetForecast(URL) {
 }
 
 function GenerateURL() {
-  let url = "http://api.weatherapi.com/v1/forecast.json?";
+  let url = "https://api.weatherapi.com/v1/forecast.json?";
   let data = {
     key: "2ff45aa75d644a10b9b112141212403",
     q: "Vienna", // Location
@@ -34,10 +34,15 @@ function ShowData(current, location, forecast) {
   });
 
   current.then((value) => {
-    timeMeasuredDisplay.innerHTML = "last updated: " + value.last_updated.substring(11, 16);
+    timeMeasuredDisplay.innerHTML = "last updated " + value.last_updated.substring(11, 16);
     currentTemperaturDisplay.innerHTML = value.temp_c + " °C";
-    currentConditionDisplay.innerHTML = value.condition.text.toLowerCase();
-    currentConditionIconDisplay.src = value.condition.icon;
+    currentConditionTextDisplay.innerHTML = value.condition.text.toLowerCase();
+    currentConditionIconDisplay.src = value.condition.code == 1000 ? "../icons/night/113.png" : value.condition.icon;
+    currentWindSpeedDisplay.innerHTML = value.wind_kph + '<span class="superSmall">kph</span>';
+    currentHumidityDisplay.innerHTML = value.humidity + '<span class="superSmall">%</span>';
+    currentVisibilityDisplay.innerHTML = value.vis_km + '<span class="superSmall">km</span>';
+    currentFeelsLike.innerHTML = "feels like " + value.feelslike_c + " °C";
+    console.log("value");
   });
 }
 
@@ -48,7 +53,11 @@ let url = GenerateURL(),
   locationDisplay = document.querySelector(".locationDisplay"),
   timeMeasuredDisplay = document.querySelector(".timeMeasuredDisplay"),
   currentTemperaturDisplay = document.querySelector(".currentTemperaturDisplay"),
-  currentConditionDisplay = document.querySelector(".currentConditionDisplay"),
-  currentConditionIconDisplay = document.querySelector(".currentConditionIconDisplay");
+  currentConditionTextDisplay = document.querySelector(".currentConditionTextDisplay"),
+  currentConditionIconDisplay = document.querySelector(".currentConditionIconDisplay"),
+  currentWindSpeedDisplay = document.querySelector(".currentWindSpeedDisplay"),
+  currentHumidityDisplay = document.querySelector(".currentHumidityDisplay"),
+  currentVisibilityDisplay = document.querySelector(".currentVisibilityDisplay"),
+  currentFeelsLike = document.querySelector(".currentFeelsLike");
 
 ShowData(currentDATA, locationDATA, forecastDATA);
