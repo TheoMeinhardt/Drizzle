8472e7e96ada42bc458e2764b873a6a27ef25d21
TheoMeinhardt <meinhardt.t04@htlwienwest.at>
2021-05-12 12:44:25 +0200

    V2021.05.12

---
+++ script/bundle.js
@@ -0,0 +1,69 @@
+(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
+async function GetLocation(URL) {
+    let response = await fetch(URL).then((Response) => Response.json());
+    return response.location;
+}
+async function GetCurrent(URL) {
+    let response = await fetch(URL).then((Response) => Response.json());
+    return response.current;
+}
+async function GetForecast(URL) {
+    let response = await fetch(URL).then((Response) => Response.json());
+    return response.forecast;
+}
+function GenerateURL() {
+    let url = "https://api.weatherapi.com/v1/forecast.json?";
+    let data = {
+        key: "2ff45aa75d644a10b9b112141212403",
+        q: "Vienna",
+        days: 3,
+        aqi: "no",
+        alerts: "no",
+    };
+    url += `key=${data.key}&q=${data.q}&days=${data.days}&aqi=${data.aqi}&alerts=${data.alerts}`;
+    return url;
+}
+// updating the data in html
+function ShowData(current, location, forecast) {
+    location.then((value) => {
+        locationDisplay.innerHTML = `${value.name}, ${value.country}`;
+    });
+    current.then((value) => {
+        timeMeasuredDisplay.innerHTML = "last updated " + value.last_updated.substring(11, 16);
+        currentTemperaturDisplay.innerHTML = value.temp_c + " °C";
+        currentConditionTextDisplay.innerHTML = value.condition.text.toLowerCase();
+        currentConditionIconDisplay.src = value.condition.code == 1000 ? "../icons/night/113.png" : value.condition.icon;
+        currentWindSpeedDisplay.innerHTML = value.wind_kph + '<span class="superSmall">kph</span>';
+        currentHumidityDisplay.innerHTML = value.humidity + '<span class="superSmall">%</span>';
+        currentVisibilityDisplay.innerHTML = value.vis_km + '<span class="superSmall">km</span>';
+        currentFeelsLike.innerHTML = "feels like " + value.feelslike_c + " °C";
+        console.log("value");
+    });
+}
+// declaring the variables
+let url = GenerateURL(), currentDATA = GetCurrent(url), locationDATA = GetLocation(url), forecastDATA = GetForecast(url), locationDisplay = document.querySelector(".locationDisplay"), timeMeasuredDisplay = document.querySelector(".timeMeasuredDisplay"), currentTemperaturDisplay = document.querySelector(".currentTemperaturDisplay"), currentConditionTextDisplay = document.querySelector(".currentConditionTextDisplay"), currentConditionIconDisplay = document.querySelector(".currentConditionIconDisplay"), currentWindSpeedDisplay = document.querySelector(".currentWindSpeedDisplay"), currentHumidityDisplay = document.querySelector(".currentHumidityDisplay"), currentVisibilityDisplay = document.querySelector(".currentVisibilityDisplay"), currentFeelsLike = document.querySelector(".currentFeelsLike"), forecastChartCanvas = document.querySelector("#forecastChart");
+let forecastChart = new Chart(forecastChartCanvas, {
+    type: "bar",
+    data: {
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        datasets: [
+            {
+                label: "# of Votes",
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: ["rgba(255, 99, 132, 0.2)", "rgba(54, 162, 235, 0.2)", "rgba(255, 206, 86, 0.2)", "rgba(75, 192, 192, 0.2)", "rgba(153, 102, 255, 0.2)", "rgba(255, 159, 64, 0.2)"],
+                borderColor: ["rgba(255, 99, 132, 1)", "rgba(54, 162, 235, 1)", "rgba(255, 206, 86, 1)", "rgba(75, 192, 192, 1)", "rgba(153, 102, 255, 1)", "rgba(255, 159, 64, 1)"],
+                borderWidth: 1,
+            },
+        ],
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true,
+            },
+        },
+    },
+});
+ShowData(currentDATA, locationDATA, forecastDATA);
+
+},{}]},{},[1]);
--- script/script.js
+++ script/script.js
@@ -1,3 +1,5 @@
+var Chart = require("chart.js");
+//#region functions
 async function GetLocation(URL) {
     let response = await fetch(URL).then((Response) => Response.json());
     return response.location;
@@ -39,6 +41,29 @@ function ShowData(current, location, forecast) {
         console.log("value");
     });
 }
+//#endregion
 // declaring the variables
-let url = GenerateURL(), currentDATA = GetCurrent(url), locationDATA = GetLocation(url), forecastDATA = GetForecast(url), locationDisplay = document.querySelector(".locationDisplay"), timeMeasuredDisplay = document.querySelector(".timeMeasuredDisplay"), currentTemperaturDisplay = document.querySelector(".currentTemperaturDisplay"), currentConditionTextDisplay = document.querySelector(".currentConditionTextDisplay"), currentConditionIconDisplay = document.querySelector(".currentConditionIconDisplay"), currentWindSpeedDisplay = document.querySelector(".currentWindSpeedDisplay"), currentHumidityDisplay = document.querySelector(".currentHumidityDisplay"), currentVisibilityDisplay = document.querySelector(".currentVisibilityDisplay"), currentFeelsLike = document.querySelector(".currentFeelsLike");
+let url = GenerateURL(), currentDATA = GetCurrent(url), locationDATA = GetLocation(url), forecastDATA = GetForecast(url), locationDisplay = document.querySelector(".locationDisplay"), timeMeasuredDisplay = document.querySelector(".timeMeasuredDisplay"), currentTemperaturDisplay = document.querySelector(".currentTemperaturDisplay"), currentConditionTextDisplay = document.querySelector(".currentConditionTextDisplay"), currentConditionIconDisplay = document.querySelector(".currentConditionIconDisplay"), currentWindSpeedDisplay = document.querySelector(".currentWindSpeedDisplay"), currentHumidityDisplay = document.querySelector(".currentHumidityDisplay"), currentVisibilityDisplay = document.querySelector(".currentVisibilityDisplay"), currentFeelsLike = document.querySelector(".currentFeelsLike"), forecastChartCanvas = document.querySelector("#forecastChart");
+let forecastChart = new Chart(forecastChartCanvas, {
+    type: "bar",
+    data: {
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        datasets: [
+            {
+                label: "# of Votes",
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: ["rgba(255, 99, 132, 0.2)", "rgba(54, 162, 235, 0.2)", "rgba(255, 206, 86, 0.2)", "rgba(75, 192, 192, 0.2)", "rgba(153, 102, 255, 0.2)", "rgba(255, 159, 64, 0.2)"],
+                borderColor: ["rgba(255, 99, 132, 1)", "rgba(54, 162, 235, 1)", "rgba(255, 206, 86, 1)", "rgba(75, 192, 192, 1)", "rgba(153, 102, 255, 1)", "rgba(255, 159, 64, 1)"],
+                borderWidth: 1,
+            },
+        ],
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true,
+            },
+        },
+    },
+});
 ShowData(currentDATA, locationDATA, forecastDATA);
